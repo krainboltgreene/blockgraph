3d10db36dfec9c00821938936907d869b8ec9aaa
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-01-31 01:19:42 -0800

    Trying to improve the concurrency issue

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_client do |config|
-  config.redis = { size: 4 }
+  config.redis = { size: 2 }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: 4 }
+  config.redis = { size: 10 }
 end
