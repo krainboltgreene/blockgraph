75960583e1b056c5209c7cdd4f4b93d2dd9b1ce0
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-02-06 13:28:31 -0800

    Handling edge case where profiles don't exist.

--- app/workers/unconnect_profile_worker.rb
+++ app/workers/unconnect_profile_worker.rb
@@ -5,8 +5,7 @@ class UnconnectProfileWorker
 
   def perform(block_id, profile_id)
     block = Block.find_by!(id: block_id)
-    profile = Profile.find_by!(id: profile_id)
 
-    block.leafs.where(profile: profile).each(&:destroy)
+    block.leafs.where(profile_id: profile_id).each(&:destroy)
   end
 end
